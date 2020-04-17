a1b5e57a0c68327dfacfa690bc754b4517b7555c
mark <markpackham1@gmail.com>
2020-04-17 13:18:46 +0100

    Ticket update and delete

--- models/ticket.rb
+++ models/ticket.rb
@@ -25,4 +25,26 @@ class Ticket
     sql = "DELETE FROM tickets"
     SqlRunner.run(sql)
   end
+
+  def save()
+    sql = "INSERT INTO tickets
+        (
+          customer_id,
+          film_id
+        )
+        VALUES
+        (
+          $1, $2
+        )
+        RETURNING id"
+    values = [@customer_id, @film_id]
+    ticket = SqlRunner.run(sql, values).first
+    @id = ticket["id"].to_i
+  end
+
+  def update()
+    sql = "UPDATE tickets SET customer_id = $1, film_id = $2 WHERE id = $3"
+    values = [@customer_id, @film_id, @id]
+    SqlRunner.run(sql, values)
+  end
 end
